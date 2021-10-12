7cb58f5c46699c537b6531f93e9bf375f9aefa02
angelrgc <angelyrgc@gmail.com>
2021-10-12 14:33:09 -0500

    Linked List Changes

--- linked_list.py
+++ linked_list.py
@@ -14,3 +14,53 @@ class Node:
 
     def __repr__(self):
         return "<Node data: %s>" % self.data
+
+class LinkedList:
+    "singly linked list"
+
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list takes 0(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count 
+
+    def add(self, data):
+        """
+        Adds new Node containing data at the head of the list 
+        Takes 0(1) time
+        """
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def __repr__(self):
+        "Returns a string representation of the list  Takes 0(n) time"
+
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head: %s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            
+            current = current.next_node
+        return '-> '.join(nodes)
+                
